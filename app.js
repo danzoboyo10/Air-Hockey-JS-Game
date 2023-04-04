1d31549c7b76c94912a09ceb0c37dcd71d32a560
Danny <you@example.com>
2023-04-03 20:19:30 -0400

    wip: working app + some styling

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
 const hockeyBoard = document.querySelector("#hockeyBoard");
 const ctx = hockeyBoard.getContext("2d"); // Context what we draw on 
-const scoreBoard = document.querySelector("#scoreBoard");
-const restartBtn = document.querySelector("restartBtn");
-const tableWidth = hockeyBoard.width;
-const tableHeight = hockeyBoard.height;
+
 const boardBackground = "white"
 
+ctx.beginPath();
+ctx.moveTo(225, 0);
+ctx.lineTo(225, 200);
+ctx.strokeStyle = "#00008B";
+ctx.stroke();
+ctx.beginPath();
+ctx.moveTo(150, 0);
+ctx.lineTo(150, 40);
+ctx.strokeStyle = "#00008B";
+ctx.stroke();
 
+ctx.font = "10px consolas, monospace";
+ctx.strokeStyle = "red";
+ctx.strokeText("By Danny Slebodnick", 90, 140);
+ctx.beginPath();
+ctx.moveTo(75, 0);
+ctx.lineTo(75, 200);
+ctx.strokeStyle = "#00008B";
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(150, 75, 35, 0, 2 * Math.PI);
+ctx.moveTo(0, 75);
+
+ctx.strokeStyle = "#00008B"
+ctx.stroke();
+
+
+const scoreBoard = document.querySelector("#scoreBoard");
+const restartBtn = document.querySelector("#restartBtn");
+
+
+const tableWidth = hockeyBoard.width;
+const tableHeight = hockeyBoard.height;
 
 
 const aPaddle1Color = "red";
 const aPaddle2Color = "red";
 const aPaddleBorder = "black";
 const puckColor = "black";
-const aPaddle1Img = "https://www.avogameroom.com/resources/apps/cart/category/215__cat.jpg"
-const puckRadius = 10;
+const puckRadius = 9;
 const aPaddleSpeed = 30; 
 let intervalID;
 let puckSpeed = 1; // lowest speed
@@ -23,7 +52,10 @@ let puckBorderColor = "black";
 let puckY = tableHeight / 2;
 let puckXDirection = 0; // direction puck goes on x axis
 let puckYDirection = 0; // direction puck goes on y axis
-let player1Score = 0; 
+
+let player1 = "PLAYER1:"
+let player2 = "PLAYER2:"
+let player1Score = 0;
 let player2Score = 0;
 
 let aPaddle1 = {
@@ -40,17 +72,13 @@ let aPaddle2 = {
 } 
 
 window.addEventListener("keydown", changeDirection); // to listen to keydown events
-// restartBtn.addEventListener("click", restartGame);
+restartBtn.addEventListener("click", restartGame);
 
 startGame();
 
 function startGame() {
   createPuck();
   nextTick();
-
-
-
-
 
 }
 function nextTick() {
@@ -63,15 +91,10 @@ function nextTick() {
     nextTick();    
   }, 10)
 
-
-
 }
 function clearTable() {
   ctx.fillStyle = boardBackground;
   ctx.fillRect(0, 0, tableWidth, tableHeight);
-
-
-
 
 }
 function drawAPaddles() {
@@ -84,8 +107,10 @@ function drawAPaddles() {
   ctx.fillStyle = aPaddle2Color;
   ctx.fillRect(aPaddle2.x, aPaddle2.y, aPaddle2.width, aPaddle2.height); 
   ctx.strokeRect(aPaddle2.x, aPaddle2.y, aPaddle2.width, aPaddle2.height)
-
+  
 }
+
+
 function createPuck() {
   puckSpeed = 1;
   if(Math.round(Math.random()) == 1) {
@@ -120,11 +145,45 @@ function drawPuck(puckX, puckY) {
   ctx.stroke();
   ctx.fill();
 
+}
+function checkCollision() {
+  if(puckY <= 0 + puckRadius) {
+    puckYDirection *= - 1;
+  }
+  if(puckY >= tableHeight - puckRadius) {
+    puckYDirection *= -1;
+  }
+  if(puckX <= 0) {
+    player2Score += 1;
+    updateScoreBoard();
+    createPuck();
+    return
+  }
 
+  if(puckX >= tableWidth) {
+    player1Score += 1;
+    updateScoreBoard();
+    createPuck();
+    return
+  }
+  //bouncing off air hockey paddles
+  if(puckX <= (aPaddle1.x + aPaddle1.width + puckRadius)) {
+    if(puckY > aPaddle1.y && puckY < aPaddle1.y + aPaddle1.height) {
+      puckX = (aPaddle1.x + aPaddle1.width + puckRadius) // if puck is stuck in corner
+      puckXDirection *= -1;
+      puckSpeed += 1;
+    }
+  }
+  if(puckX >= (aPaddle2.x - puckRadius)) {
+    if(puckY > aPaddle2.y && puckY < aPaddle2.y + aPaddle2.height) {
+      puckX = aPaddle2.x - puckRadius // if puck is stuck
+      puckXDirection *= -1;
+      puckSpeed += 1;
+    }
 
+  }
 
 }
-function checkCollision() {}
 
 function changeDirection(event) {
   const keyPressed = event.keyCode;
@@ -158,12 +217,34 @@ function changeDirection(event) {
   }
 
 };
+function updateScoreBoard() {
+  scoreBoard.textContent = `${player1}${player1Score}\n ${player2}${player2Score}`
+}
+function restartGame() {
+  player1Score = 0;
+  player2Score= 0;
+  aPaddle1 = {
+    width: 10, 
+    height: 30, 
+    x:0, 
+    y:0
+  }; 
+  aPaddle2 = {
+    width: 10, 
+    height: 30, 
+    x: tableWidth - 10,
+    y: tableHeight - 30
+    };
+    puckSpeed = 1;
+    puckX = 0; 
+    puckY = 0;
+    puckXDirection = 0;
+    updateScoreBoard();
+    clearInterval(intervalID);
+    startGame();
+};
 
-
-
-function UpdateScoreBoard() {}
-function restartGame() {}
-
+console.log(restartGame)
 
 
 
